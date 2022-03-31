433ba3d662feb773f5b5a57b2c18cf8a374fd61a
Simon <simon.schoeggler@hotmail.com>
2022-03-31 22:35:00 +0200

    bt 5 update

--- src/main/webapp/js/mh.js
+++ src/main/webapp/js/mh.js
@@ -1,4 +1,8 @@
 let _jwt;
+let _startCounter;
+let _endCounter;
+
+let _movies;
 
 function searchMovie(searchString, page) {
 
@@ -8,59 +12,86 @@ function searchMovie(searchString, page) {
             }
         })*/
 
+    _startCounter = 0;
+    _endCounter = 5;
+
     if (searchString !== "") {
         fetch('./api/search/' + searchString + '/' + page)
             .then(value => {
                 console.log(value);
                 value.json().then(data => {
+                    _movies = data;
                     console.log(data);
-                    document.getElementById('searchResult').innerHTML = '<a id="leftSlide" href="#">\n' +
-                        '        <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" fill="currentColor" class="bi bi-caret-left" viewBox="0 0 16 16">\n' +
-                        '  <path d="M10 12.796V3.204L4.519 8 10 12.796zm-.659.753-5.48-4.796a1 1 0 0 1 0-1.506l5.48-4.796A1 1 0 0 1 11 3.204v9.592a1 1 0 0 1-1.659.753z"/>\n' +
-                        '</svg>\n' +
-                        '    </a>\n' +
-                        '    <ul style="text-align: center; margin-top: 25px" id="list" class="list-group list-group-horizontal">\n' +
-                        '    </ul>\n' +
-                        '    <a id="rightSlide" href="#">\n' +
-                        '        <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" fill="currentColor" class="bi bi-caret-right" viewBox="0 0 16 16">\n' +
-                        '  <path d="M6 12.796V3.204L11.481 8 6 12.796zm.659.753 5.48-4.796a1 1 0 0 0 0-1.506L6.66 2.451C6.011 1.885 5 2.345 5 3.204v9.592a1 1 0 0 0 1.659.753z"/>\n' +
-                        '</svg>\n' +
-                        '    </a>';
-                    document.getElementById('list').innerHTML = '';
-                    let counter = 0;
-
-                    data.results.forEach(movie => {
-                        if (counter < 5) {
-
-                            let img = '';
-                            if (movie.poster_path == null) {
-                                img = 'images/notAvailable.jpg'
-                            } else {
-                                img = 'https://image.tmdb.org/t/p/original' + movie.poster_path;
-                            }
-
-                            let card = '<div class="card" style="width:200px">\n' +
-                                '  <div style="height: 300px">\n' +
-                                '  <img class="card-img mx-auto d-block" src="' + img + '" alt="Card image">\n' +
-                                '  </div>\n' +
-                                '  <div class="card-body">\n' +
-                                '    <h4 style="height: 60px" class="card-title">' + movie.original_title + '</h4>\n' +
-                                '<hr>' +
-                                '    <p style="height: 125px" class="card-text">' + movie.overview.substring(0, 100) + '...' + '</p>\n' +
-                                '    <a href="#" onclick="getTMDBInformation(' + movie.id + ')" class="btn btn-primary">See More</a>\n' +
-                                '  </div>\n' +
-                                '</div>';
-                            let item = '<li class="list-group-item">' + card + '</li>';
-                            document.getElementById('list').innerHTML += item;
-                            counter++;
-                        }
-                    })
+                   loadMovies();
                 })
             })
     } else {
         document.getElementById('searchResult').innerHTML = '';
     }
 }
+
+function nextMovie() {
+    if (_endCounter < 19) {
+        _startCounter++;
+        _endCounter++;
+
+        loadMovies();
+    }
+}
+
+function previousMovie () {
+    if (_startCounter > 0) {
+        _startCounter--;
+        _endCounter--;
+
+        loadMovies();
+    }
+}
+
+function loadMovies () {
+    document.getElementById('searchResult').innerHTML = '<button class="btn bg-transparent" id="leftSlide" onclick="previousMovie()">\n' +
+        '        <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" fill="currentColor" class="bi bi-caret-left" viewBox="0 0 16 16">\n' +
+        '  <path d="M10 12.796V3.204L4.519 8 10 12.796zm-.659.753-5.48-4.796a1 1 0 0 1 0-1.506l5.48-4.796A1 1 0 0 1 11 3.204v9.592a1 1 0 0 1-1.659.753z"/>\n' +
+        '</svg>\n' +
+        '    </button>\n' +
+        '    <ul style="text-align: center; margin-top: 25px" id="list" class="list-group list-group-horizontal">\n' +
+        '    </ul>\n' +
+        '    <button class="btn bg-transparent" id="rightSlide" onclick="nextMovie()">\n' +
+        '        <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" fill="currentColor" class="bi bi-caret-right" viewBox="0 0 16 16">\n' +
+        '  <path d="M6 12.796V3.204L11.481 8 6 12.796zm.659.753 5.48-4.796a1 1 0 0 0 0-1.506L6.66 2.451C6.011 1.885 5 2.345 5 3.204v9.592a1 1 0 0 0 1.659.753z"/>\n' +
+        '</svg>\n' +
+        '    </button>';
+    document.getElementById('list').innerHTML = '';
+    let counter = 0;
+
+    _movies.results.forEach(movie => {
+        if (counter >= _startCounter && counter < _endCounter) {
+
+            let img = '';
+            if (movie.poster_path == null) {
+                img = 'images/notAvailable.jpg'
+            } else {
+                img = 'https://image.tmdb.org/t/p/original' + movie.poster_path;
+            }
+
+            let card = '<div class="card" style="width:200px">\n' +
+                '  <div style="height: 300px">\n' +
+                '  <img class="card-img mx-auto d-block" src="' + img + '" alt="Card image">\n' +
+                '  </div>\n' +
+                '  <div class="card-body">\n' +
+                '    <h4 style="height: 60px" class="card-title">' + movie.original_title + '</h4>\n' +
+                '<hr>' +
+                '    <p style="height: 125px" class="card-text">' + movie.overview.substring(0, 100) + '...' + '</p>\n' +
+                '    <a href="#" onclick="getTMDBInformation(' + movie.id + ')" class="btn btn-primary">See More</a>\n' +
+                '  </div>\n' +
+                '</div>';
+            let item = '<li class="list-group-item">' + card + '</li>';
+            document.getElementById('list').innerHTML += item;
+        }
+        counter++;
+    })
+}
+
 
 function getTMDBInformation(tmdbID) {
     fetch('./api/search/' + tmdbID)
@@ -84,6 +115,12 @@ function getIMDBInformation(imdbID) {
 let _genres;
 
 function loadGenres() {
+
+    $("#releaseYear").datepicker({
+        format: "yyyy",
+        viewMode: "years",
+        minViewMode: "years"
+    });
     fetch('./api/genres')
         .then(result => {
             result.json().then(data => {
@@ -125,6 +162,7 @@ function login(tfEmail, tfPassword) {
         _jwt = response.headers.get("Authorization");
     });
 }
+
 
 
 
