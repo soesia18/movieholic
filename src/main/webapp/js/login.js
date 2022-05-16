85047f6afb90cce0cb790de24dd88c98ca7effdb
moinity <david-brannan@outlook.at>
2022-05-16 15:41:02 +0200

    fast logout

--- src/main/webapp/js/login.js
+++ src/main/webapp/js/login.js
@@ -31,11 +31,13 @@ signUp.addEventListener("click", (e) => {
                     email: email
                 });
                 $('#registerModal').modal('hide');
+                clearLoginModal();
             })
             .catch((error) => {
                 const errorCode = error.code;
                 const errorMessage = error.message;
                 console.log("Error: " + errorMessage);
+                console.log(errorCode);
             });
     }
 });
@@ -54,12 +56,16 @@ signIn.addEventListener("click", (e) => {
             });
 
             $('#loginModal').modal('hide');
+            clearLoginModal();
         })
         .catch((error) => {
             const errorCode = error.code;
             const errorMessage = error.message;
-
             console.log("Error: " + errorMessage);
+
+            if (errorCode === 'auth/invalid-email'){
+
+            }
         });
 });
 
@@ -71,21 +77,38 @@ onAuthStateChanged(auth, (user) => {
         let navbarContent = document.getElementById("navbarContent");
         let child = document.getElementById("loginSector");
         navbarContent.removeChild(child);
-        navbarContent.innerHTML += '<div class="dropdown">\n' +
+        navbarContent.innerHTML += '<div class="dropdown loginDropdown" id="loginSector">\n' +
             '  <button type="button" class="btn btn-light dropdown-toggle" data-bs-toggle="dropdown">\n'+
             user.email + '\n' +
             '  </button>\n' +
             '  <ul class="dropdown-menu">\n' +
             '    <li><a class="dropdown-item" href="#">Account</a></li>\n' +
             '    <li><a class="dropdown-item" href="#">Settings</a></li>\n' +
-            '    <li onclick="logout()"><a class="dropdown-item" href="#">Logout</a></li>\n' +
+            '    <li id="logout"><a class="dropdown-item" href="#">Logout</a></li>\n' +
             '  </ul>\n' +
             '</div>';
+        let log = document.getElementById("logout");
+        log.addEventListener("click", (() => {
+            signOut(auth).then(() => {
+                console.log("logout");
+            }).catch((error) => {
+                console.log(error);
+            });
+        }));
     } else {
         console.log("User is signed out!");
+        let navbarContent = document.getElementById("navbarContent");
+        let child = document.getElementById("loginSector");
+        navbarContent.removeChild(child);
+
+        navbarContent.innerHTML += '<div class="nav-item navLogin" data-bs-toggle="modal" data-bs-target="#loginModal" id="loginSector">\n' +
+            '            <span id="loginSpan">Login</span>\n' +
+            '            <svg id="loginImg" xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor"\n' +
+            '                 class="bi bi-person-plus" viewBox="0 0 16 16">\n' +
+            '                <path d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H1s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C9.516 10.68 8.289 10 6 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>\n' +
+            '                <path fill-rule="evenodd"\n' +
+            '                      d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5z"/>\n' +
+            '            </svg>\n' +
+            '        </div>';
     }
 });
-
-function logout(){
-    console.log("logout");
-}
--- src/main/webapp/js/mh.js
+++ src/main/webapp/js/mh.js
@@ -428,23 +428,3 @@ function clearLoginModal() {
     document.getElementById("tfLoginEmail").value = "";
     document.getElementById("tfLoginPassword").value = "";
 }
-() => {
-    const auth = getAuth();
-    signOut(auth).then(() => {
-        let navbarContent = document.getElementById(`navbarContent`);
-        let child = document.getElementById(`loginSector`);
-        navbarContent.removeChild(child);
-
-        navbarContent.innerHTML += `<div class="nav-item navLogin" data-bs-toggle="modal" data-bs-target="#loginModal" id="loginSector">\n' +
-            '            <span id="loginSpan">Login</span>\n' +
-            '            <svg id="loginImg" xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor"\n' +
-            '                 class="bi bi-person-plus" viewBox="0 0 16 16">\n' +
-            '                <path d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H1s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C9.516 10.68 8.289 10 6 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>\n' +
-            '                <path fill-rule="evenodd"\n' +
-            '                      d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5z"/>\n' +
-            '            </svg>\n' +
-            '        </div>`;
-    }).catch((error) => {
-        console.log(error);
-    });
-};
