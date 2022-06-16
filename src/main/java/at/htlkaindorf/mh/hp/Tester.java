62b1fe757afcff5d23d691a0f11a14b156f74535
Simon <simon.schoeggler@hotmail.com>
2022-06-16 15:23:39 +0200

    javadoc

--- src/main/java/at/htlkaindorf/mh/hp/Tester.java
+++ src/main/java/at/htlkaindorf/mh/hp/Tester.java
@@ -2,6 +2,13 @@ package at.htlkaindorf.mh.hp;
 
 import java.util.List;
 
+/**
+ * <b>Movieholic</b><br><br>
+ * Test the Homepage class
+ * @author Simon
+ * @version 1.0
+ * @since last update: 23.05.2022
+ */
 public class Tester {
     public static void main(String[] args) {
         HomePage hp = HomePage.builder().userUID("Test").addHomePageItems("/discover/test", "/search/test").build();
